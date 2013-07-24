a47d252d8f99024c422bc4b1eef949fd8794b66f
WanWizard <wanwizard@wanwizard.eu>
2013-07-24 17:30:15 +0200

    added Welcome views

--- classes/Controller/Welcome.php
+++ classes/Controller/Welcome.php
@@ -4,14 +4,20 @@ namespace Controller;
 
 class Welcome extends \Controller\Base
 {
+	/**
+	 * The basic welcome message
+	 *
+	 * @access  public
+	 * @return  Response
+	 */
 	public function actionIndex()
 	{
-		return 'From Welcome::index, we say: Hello World!'.
-			'<hr>Exec time: {exec_time} - Mem usage: {mem_usage} - Mem peak usage: {mem_peak_usage}';
+		return $this->app->getViewManager()->forge('welcome/index');
 	}
 
 	public function actionView()
 	{
-		return 'From Welcome::view, we say: Hello World!';
+		return 'From Welcome::view, we say: Hello World!'.
+			'<hr>Exec time: {exec_time} - Mem usage: {mem_usage} - Mem peak usage: {mem_peak_usage}';
 	}
 }
