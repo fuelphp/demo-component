b0c0c66c6b0f786ee8d8826621db80c0d24449d1
WanWizard <wanwizard@wanwizard.eu>
2013-08-06 21:44:06 +0200

    added csrf settings to the security config file

--- config/security.php
+++ config/security.php
@@ -16,29 +16,49 @@
  */
 
 return array(
-	/**
-	 * If true, every HTTP request of the type speficied in autoload_methods
-	 * will be checked for a CSRF token. If not present or not valid, a
-	 * security exception will be thrown.
-	 */
-	'csrf_autoload'         => false,
-	'csrf_autoload_methods' => array('post', 'put', 'delete'),
 
 	/**
-	 * Name of the form field that holds the CSRF token.
+	 * CSRF protection
 	 */
-	'csrf_token_key'        => 'fuel_csrf_token',
+	'csrf' => array(
 
-	/**
-	 * Expiry of the token in seconds. If zero, the token remains the same
-	 * for the entire user session.
-	 */
-	'csrf_expiration'       => 0,
+		/**
+		 * If true, every page containing a form that POSTs data will be
+		 * automatically protected by the framework against CSRF attacks
+		 */
+		'enabled' => true,
 
-	/**
-	 * A salt to make sure the generated security tokens are not predictable
-	 */
-	'token_salt'            => 'put your salt value here to make the token more secure',
+		/**
+		 * The Security Csrf driver used to provide the protection mechanism.
+		 * The framework provides the following drivers:
+		 * noop - for testing purposes, uses a fixed test-token, and validates all responses
+		 * session - uses a single token per user-session, with optional expiration/rotation
+		 * page - protects individual pages with unique tokens, requires cookie and javascript support
+		 * form - protects individual forms with unique tokens, requires cookie and javascript support
+		 */
+		'driver' => 'session',
+
+		/**
+		 * A salt to make sure the generated security tokens are not predictable
+		 */
+		'salt' => 'put your salt value here to make the token more secure',
+
+		/**
+		 * list of HTTP methods that will require CSRF protection
+		 */
+		'http_methods' => array('post', 'put', 'delete'),
+
+		/**
+		 * Name of the form field that holds the CSRF token.
+		 */
+		'token_key'        => 'fuel_csrf_token',
+
+		/**
+		 * Expiry of the token in seconds. If zero, the token remains the same
+		 * for the entire user session.
+		 */
+		'expiration'       => 0,
+	),
 
 	/**
 	 * Allow the Input class to use X headers when present
@@ -73,13 +93,18 @@ return array(
 	 * How much is dependant on how much input data there is.
 	 */
 	'output_filter'  => array(
-		'htmlentities'
+		'HtmlEntities'
 	),
 
 	/**
 	 * Encoding mechanism to use on htmlentities()
 	 */
 	'htmlentities_flags' => ENT_QUOTES,
+
+	/**
+	 * Encoding to be used when converting
+	 */
+	'htmlentities_encoding' => 'UTF-8',
 
 	/**
 	 * Wether to encode HTML entities as well
